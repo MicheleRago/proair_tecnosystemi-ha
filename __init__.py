d9b591c6322ee4ffeee1f91666a781b861d29b1d
Michele Rago <michele@rago.ovh>
2026-01-13 11:43:55 +0100

    Move Humidity

--- __init__.py
+++ __init__.py
@@ -27,14 +27,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
     
-    # Avviamo la piattaforma climate
-    await hass.config_entries.async_forward_entry_setups(entry, ["climate"])
+    # Avviamo la piattaforma climate e sensor
+    await hass.config_entries.async_forward_entry_setups(entry, ["climate", "sensor"])
     
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Rimuove l'integrazione e pulisce la memoria."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["climate"])
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["climate", "sensor"])
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
---
+++ sensor.py
@@ -0,0 +1,55 @@
+import logging
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorDeviceClass,
+    SensorStateClass,
+)
+from homeassistant.const import PERCENTAGE
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up the ProAir sensor platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    
+    if coordinator.data and "Zones" in coordinator.data:
+        sensors = []
+        for zone in coordinator.data["Zones"]:
+            # Check if humidity data is available (some older zones might not have it)
+            if zone.get("Umd") is not None:
+                sensors.append(ProAirHumiditySensor(coordinator, zone))
+        
+        if sensors:
+            async_add_entities(sensors, True)
+    
+class ProAirHumiditySensor(CoordinatorEntity, SensorEntity):
+    """Representation of a ProAir Humidity Sensor."""
+
+    _attr_device_class = SensorDeviceClass.HUMIDITY
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_native_unit_of_measurement = PERCENTAGE
+
+    def __init__(self, coordinator, zone_data):
+        super().__init__(coordinator)
+        self._id = zone_data["ZoneId"]
+        self._name = zone_data["Name"]
+        self._attr_unique_id = f"proair_{coordinator.api.serial}_{zone_data['ZoneId']}_humidity"
+        self._attr_name = f"{zone_data['Name']} Humidity"
+
+    @property
+    def _zone_data(self):
+        """Get updated zone data."""
+        for zone in self.coordinator.data.get("Zones", []):
+            if zone["ZoneId"] == self._id:
+                return zone
+        return {}
+
+    @property
+    def native_value(self):
+        """Return the state of the sensor."""
+        val = self._zone_data.get("Umd")
+        if val is not None:
+            return float(val) / 10
+        return None
